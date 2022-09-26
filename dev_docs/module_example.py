46fdf6245077a1b7eb204223c4f91827f92d2d29
Chad Lowe <pfmsoft.dev@gmail.com>
2022-09-26 16:27:01 -0700

    code snippets and module header example

---
+++ dev_docs/module_example.py
@@ -0,0 +1,10 @@
+####################################################
+#                                                  #
+#          dev_docs/module_example.py
+#                                                  #
+####################################################
+# Created by: Chad Lowe                            #
+# Created on: 2022-09-26T16:19:30-07:00            #
+# Last Modified: _iso_date_         #
+# Source: https://github.com/DonalChilde/snippets  #
+####################################################
