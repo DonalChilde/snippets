78b0c5553caa6293c2de2978ddccd5a0c22d7551
Chad Lowe <pfmsoft.dev@gmail.com>
2022-09-27 10:16:24 -0700

    argparse test stubs

---
+++ tests/snippets/argparse/help_on_error_test.py
@@ -0,0 +1,16 @@
+####################################################
+#                                                  #
+#   tests/snippets/argparse/help_on_error_test.py
+#                                                  #
+####################################################
+# Created by: Chad Lowe                            #
+# Created on: 2022-09-27T10:14:32-07:00            #
+# Last Modified: _iso_date_         #
+# Source: https://github.com/DonalChilde/snippets  #
+####################################################
+
+"""Tests for ArgumentParserHelpOnError"""
+
+from snippets.argparse.help_on_error import ArgumentParserHelpOnError
+
+# TODO Write Tests
---
+++ tests/snippets/argparse/readable_directory_test.py
@@ -0,0 +1,16 @@
+####################################################
+#                                                  #
+#  tests/snippets/argparse/readable_directory_test.py
+#                                                  #
+####################################################
+# Created by: Chad Lowe                            #
+# Created on: 2022-09-27T10:11:29-07:00            #
+# Last Modified: _iso_date_         #
+# Source: https://github.com/DonalChilde/snippets  #
+####################################################
+"""
+tests for ReadableDirectory
+"""
+from snippets.argparse.readable_directory import ReadableDirectory
+
+# TODO write tests
