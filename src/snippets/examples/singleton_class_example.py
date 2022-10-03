351e4e97532d386b4a0c53420fc3b4a3f7c8efaf
Chad Lowe <pfmsoft.dev@gmail.com>
2022-10-03 11:40:58 -0700

    added new singleton examples, with links

--- src/snippets/examples/singleton_class_example.py
+++ src/snippets/examples/singleton_class_example.py
@@ -1,5 +1,25 @@
 class SingletonClass(object):
+    """
+    Dont use hasattr?
+    https://stackoverflow.com/a/11517201/105844
+    """
+
     def __new__(cls):
-        if not hasattr(cls, "instance"):
-            cls.instance = super(SingletonClass, cls).__new__(cls)
-        return cls.instance
+        if not hasattr(cls, "_instance"):
+            cls._instance = super(SingletonClass, cls).__new__(cls)
+        return cls._instance
+
+
+class Singleton(object):
+    """https://www.python.org/download/releases/2.2/descrintro/#__new__"""
+
+    def __new__(cls, *args, **kwds):
+        it = cls.__dict__.get("__it__")
+        if it is not None:
+            return it
+        cls.__it__ = it = object.__new__(cls)
+        it.init(*args, **kwds)
+        return it
+
+    def init(self, *args, **kwds):
+        pass
