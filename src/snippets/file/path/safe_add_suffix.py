356e410c7841affc40ec7af1bea315f941b6e459
Chad Lowe <pfmsoft.dev@gmail.com>
2022-12-03 16:49:46 -0700

    validate filepath

--- src/snippets/file/path/safe_add_suffix.py
+++ src/snippets/file/path/safe_add_suffix.py
@@ -4,3 +4,13 @@ from pathlib import Path
 def safe_add_suffix(path: Path, suffix: str) -> Path:
     """Add a suffix to a path without clobbering an existing suffix"""
     return Path(f"{path}.{suffix}")
+
+
+def append_suffix(path: Path, suffix: str) -> Path:
+    """Append a value to the last part of a path.
+
+    Can be used to append a suffix to a filename, without removing the old suffix.
+    """
+    if not path.parts:
+        raise ValueError(f"{path} has no parts. Was Path('') used to make it?")
+    return path.parent / f"{path.parts[-1]}{suffix}"
--- src/snippets/file/validate_file_out.py
+++ src/snippets/file/validate_file_out.py
@@ -5,20 +5,29 @@
 ####################################################
 # Created by: Chad Lowe                            #
 # Created on: 2022-10-29T09:11:28-07:00            #
-# Last Modified: _iso_date_         #
+# Last Modified: 2022-12-03T23:49:38.693575+00:00  #
 # Source: https://github.com/DonalChilde/snippets  #
 ####################################################
 from pathlib import Path
 
 
-def validate_file_out(output_path: Path, overwrite: bool):
-    """Ensure that a file path is suitable for output."""
-    if output_path.is_dir():
+def validate_file_out(
+    file_path: Path, overwrite: bool, *, ensure_parent: bool = True
+) -> bool:
+    """Ensure that a file path is suitable for output.
+
+    Optionally can ensure that parent directories exist.
+    """
+    if file_path.is_dir():
         raise ValueError(
-            f"{output_path} is an invalid destination. It is an existing directory."
+            f"{file_path} is an invalid destination. It is an existing directory."
         )
-    if not overwrite and output_path.is_file():
+    if not overwrite and file_path.is_file():
         raise ValueError(
-            f"{output_path} is an invalid destination. "
+            f"{file_path} is an invalid destination. "
             "It is an existing file and overwrite was not selected."
         )
+    if ensure_parent:
+        parent = file_path.parent
+        parent.mkdir(parents=True, exist_ok=True)
+    return file_path.parent.is_dir()
