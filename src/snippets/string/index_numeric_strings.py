d39f10d76ea49f0f34ffedcb589a6155a788c168
Chad Lowe <pfmsoft.dev@gmail.com>
2022-12-03 18:04:20 -0700

    use indexedstring from parsing

--- src/snippets/string/index_numeric_strings.py
+++ src/snippets/string/index_numeric_strings.py
@@ -5,16 +5,15 @@
 ####################################################
 # Created by: Chad Lowe                            #
 # Created on: 2022-11-02T07:27:43-07:00            #
-# Last Modified: _iso_date_         #
+# Last Modified: 2022-12-04T01:04:14.715215+00:00  #
 # Source: https://github.com/DonalChilde/snippets  #
 ####################################################
 from typing import Iterable, Iterator
 
-from snippets.string.indexed_string import IndexedString
+from snippets.parsing.indexed_string import IndexedString
 
 
 def index_numeric_strings(string_values: Iterable[str]) -> Iterator[IndexedString]:
-    """With an"""
     for idx, item in enumerate(string_values):
         if item.isnumeric():
             yield IndexedString(idx, item)
