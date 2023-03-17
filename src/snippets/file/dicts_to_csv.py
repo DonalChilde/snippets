4f4f92beb7131bb9f2a7ef198dbc4f138aca3b00
Chad Lowe <pfmsoft.dev@gmail.com>
2023-03-17 14:23:45 -0700

    remove more_itertool dependency, use validate_file_out

--- src/snippets/file/dicts_to_csv.py
+++ src/snippets/file/dicts_to_csv.py
@@ -5,67 +5,53 @@
 ####################################################
 # Created by: Chad Lowe                            #
 # Created on: 2022-10-13T11:41:58-07:00            #
-# Last Modified: _iso_date_         #
+# Last Modified: 2023-03-17T21:23:38.160492+00:00  #
 # Source: https://github.com/DonalChilde/snippets  #
 ####################################################
 
 import csv
 from pathlib import Path
-from typing import Any, Dict, Iterable, Literal, Optional, Sequence
+from typing import Dict, Iterable
 
-try:
-    from more_itertools import peekable
-except ImportError:
-    from snippets.vendor.peekable import peekable  # type: ignore
+from snippets.file.validate_file_out import validate_file_out
 
 
 def dicts_to_csv(
     dicts: Iterable[Dict],
     output_path: Path,
     *,
-    parents: bool = True,
     overwrite_ok: bool = False,
     write_header: bool = True,
-    fields: Optional[Sequence] = None,
-    restval: Any = "",
+    **kwargs,
 ):
     """
     Save an iterable of dicts to csv.
 
-    If a list of fields is not provided, all fields will be output, in the order
-    they are defined in the dict.
+    The file output path will be checked for validity. Existing files will not be
+    overwritten unless overwrite_ok = True. Invalid file paths will throw a ValueError.
+    Parent directories will be created as necessary.
 
     Args:
-        dicts: _description_
-        output_path: Output path to the csv file.
-        parents: Create missing parent directories. Defaults to True.
+        dicts: the dicts to save.
+        output_path: The file path to the csv.
         overwrite_ok: Overwrite an existing file. Defaults to False.
-        write_header: First line of the csv file contains column headers.
-            Defaults to True.
-        fields: Ordered list of fields to be output. Defaults to None.
-        restval: Default value for missing field. Defaults to "".
+        write_header: Write the first line as field headers. Defaults to True.
+
+    Raises:
+        ValueError: Raised for invalid file paths, or no dicst to save.
     """
-    if overwrite_ok:
-        mode = "w"
-    else:
-        mode = "x"
-    if parents:
-        output_path.parent.mkdir(parents=parents, exist_ok=True)
-    source = peekable(dicts)
-    if fields is None:
-        first = source.peek()
-        fieldnames: Sequence = list(first.keys())
-    else:
-        fieldnames = fields
-    if fields is not None:
-        extras: Literal["raise", "ignore"] = "ignore"
-    else:
-        extras = "raise"
-    with open(output_path, mode, newline="", encoding="utf-8") as csv_file:
-        writer = csv.DictWriter(
-            csv_file, fieldnames=fieldnames, extrasaction=extras, restval=restval
-        )
+    validate_file_out(output_path, overwrite=overwrite_ok, ensure_parent=True)
+    iter_dict = iter(dicts)
+    try:
+        first = next(iter_dict)
+    except StopIteration as exc:
+        raise ValueError("No data available to save as csv.") from exc
+    fieldnames = kwargs.pop("fieldnames", [])
+    if not fieldnames:
+        fieldnames = list(first.keys())
+    with open(output_path, "w", newline="", encoding="utf-8") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames, **kwargs)
         if write_header:
             writer.writeheader()
-        for row in source:
-            writer.writerow(row)
+        writer.writerow(first)
+        writer.writerows(iter_dict)
