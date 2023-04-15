1495d327cd528eb72ece9250ad876facd9d3731f
Chad Lowe <pfmsoft.dev@gmail.com>
2023-04-15 15:18:34 -0700

    simplify count store

--- src/snippets/file/line_count.py
+++ src/snippets/file/line_count.py
@@ -5,15 +5,15 @@
 ####################################################
 # Created by: Chad Lowe                            #
 # Created on: 2022-12-03T12:11:37-07:00            #
-# Last Modified: 2022-12-03T23:43:00.749902+00:00  #
+# Last Modified: 2023-04-15T22:18:24.336105+00:00  #
 # Source: https://github.com/DonalChilde/snippets  #
 ####################################################
 from pathlib import Path
 
 
 def line_count(file_name: Path) -> int:
-    total = 0
+    count = 0
     with file_name.open(mode="rt", encoding="utf-8") as file_in:
         for count, _ in enumerate(file_in, start=1):
-            total = count
-    return total
+            pass
+    return count
