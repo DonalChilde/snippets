f9cf666ea369f5e04f6a33eb8f11821a5d44b7d5
Chad Lowe <pfmsoft.dev@gmail.com>
2023-05-22 10:19:00 -0700

    update init

--- src/snippets/messages/__init__.py
+++ src/snippets/messages/__init__.py
@@ -5,11 +5,17 @@
 ####################################################
 # Created by: Chad Lowe                            #
 # Created on: 2023-05-22T07:07:59-07:00            #
-# Last Modified: 2023-05-22T14:13:38.638356+00:00  #
+# Last Modified: 2023-05-22T17:18:56.334481+00:00  #
 # Source: https://github.com/DonalChilde/snippets  #
 ####################################################
 from .message import Message
 from .message_publisher import MessagePublisher
-from .messenge_listener import MessengeListener, PrintMessengeListener
+from .messenge_listener import MessengeListener, PrintMessengeListener, category_sieve
 
-__all__ = ["MessagePublisher", "Message", "PrintMessengeListener", "MessengeListener"]
+__all__ = [
+    "MessagePublisher",
+    "Message",
+    "PrintMessengeListener",
+    "MessengeListener",
+    "category_sieve",
+]
