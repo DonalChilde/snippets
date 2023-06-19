d77575146bd8b7ecbcbb864f5add3d8108ad56f3
Chad Lowe <pfmsoft@gmail.com>
2023-06-19 07:26:49 -0700

    add constants.time module

---
+++ src/snippets/constants/time.py
@@ -0,0 +1,12 @@
+####################################################
+#                                                  #
+#          src/snippets/constants/time.py
+#                                                  #
+####################################################
+# Created by: Chad Lowe                            #
+# Created on: 2023-06-19T07:22:57-07:00            #
+# Last Modified: 2023-06-19T14:26:45.692696+00:00  #
+# Source: https://github.com/DonalChilde/snippets  #
+####################################################
+
+NANOS_PER_SECOND = 1000000000
