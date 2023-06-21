eeca490e62c48228be0b17a84f07209da987e904
Chad Lowe <pfmsoft@gmail.com>
2023-06-20 19:39:37 -0700

    add composable func timer

--- src/snippets/timers/function_timer.py
+++ src/snippets/timers/function_timer.py
@@ -5,13 +5,14 @@
 ####################################################
 # Created by: Chad Lowe                            #
 # Created on: 2023-06-19T07:38:54-07:00            #
-# Last Modified: 2023-06-20T23:52:49.112043+00:00  #
+# Last Modified: 2023-06-21T02:39:31.473449+00:00  #
 # Source: https://github.com/DonalChilde/snippets  #
 ####################################################
 
 import logging
 from functools import wraps
 from time import perf_counter_ns
+from typing import Any, Callable, Tuple
 
 _logger = logging.getLogger(__name__)
 _logger.addHandler(logging.NullHandler())
@@ -65,3 +66,61 @@ def function_timer(
         return wrapper
 
     return decorator
+
+
+def timer_ns(callback: Callable[[int, int, str, tuple[tuple, dict]], None]):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = perf_counter_ns()
+            result = func(*args, **kwargs)
+            end = perf_counter_ns()
+            callback(start, end, func.__qualname__, (args, kwargs))
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+class TimeLogger:
+    def __init__(
+        self,
+        logger: logging.Logger | None = None,
+        level: int = logging.DEBUG,
+        repr_length: int = 100,
+    ) -> None:
+        if logger is None:
+            self.logger = _logger
+        else:
+            self.logger = logger
+        self.level = level
+        self.repr_length = repr_length
+
+    def __call__(
+        self, start: int, end: int, func_name: str, func_args: tuple[tuple, dict]
+    ) -> Any:
+        if self.repr_length == -1:
+            self.logger.log(
+                self.level,
+                "%s ran in %ss with args: %s",
+                func_name,
+                f"{(end-start)/NANOS_PER_SECOND:9f}",
+                repr(func_args),
+            )
+
+        elif self.repr_length > 0:
+            self.logger.log(
+                self.level,
+                "%s ran in %ss with args: %s",
+                func_name,
+                f"{(end-start)/NANOS_PER_SECOND:9f}",
+                repr(func_args)[: self.repr_length],
+            )
+        else:
+            self.logger.log(
+                self.level,
+                "%s ran in %ss",
+                func_name,
+                f"{(end-start)/NANOS_PER_SECOND:9f}",
+            )
