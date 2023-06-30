89cfab53f7907dcfbecfceefef5b40cbb331d93e
Chad Lowe <pfmsoft@gmail.com>
2023-06-30 03:29:14 -0700

    fix rel path reference,unused imports

--- src/snippets/timers/timers.py
+++ src/snippets/timers/timers.py
@@ -1,18 +1,18 @@
 ####################################################
 #                                                  #
-#      src/snippets/timers/function_timer.py
+#      src/snippets/timers/timers.py
 #                                                  #
 ####################################################
 # Created by: Chad Lowe                            #
 # Created on: 2023-06-19T07:38:54-07:00            #
-# Last Modified: 2023-06-30T10:25:18.339131+00:00  #
+# Last Modified: 2023-06-30T10:29:08.216159+00:00  #
 # Source: https://github.com/DonalChilde/snippets  #
 ####################################################
 
 import logging
 from functools import wraps
 from time import perf_counter_ns
-from typing import Any, Callable, Tuple
+from typing import Any, Callable
 
 _logger = logging.getLogger(__name__)
 _logger.addHandler(logging.NullHandler())
@@ -20,52 +20,52 @@ _logger.addHandler(logging.NullHandler())
 NANOS_PER_SECOND = 1000000000
 
 
-def function_timer(
-    logger: logging.Logger | None = None,
-    level: int = logging.DEBUG,
-    repr_length: int = 100,
-):
-    """Log the time it takes to run a function.
-    https://stackoverflow.com/a/42581103
-    """
-    if logger is None:
-        logger = _logger
+# def function_timer(
+#     logger: logging.Logger | None = None,
+#     level: int = logging.DEBUG,
+#     repr_length: int = 100,
+# ):
+#     """Log the time it takes to run a function.
+#     https://stackoverflow.com/a/42581103
+#     """
+#     if logger is None:
+#         logger = _logger
 
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            start = perf_counter_ns()
-            result = func(*args, **kwargs)
-            end = perf_counter_ns()
-            if repr_length == -1:
-                logger.log(
-                    level,
-                    "%s ran in %ss with args: %s",
-                    func.__name__,
-                    f"{(end-start)/NANOS_PER_SECOND:9f}",
-                    repr((args, kwargs)),
-                )
-                return result
-            if repr_length > 0:
-                logger.log(
-                    level,
-                    "%s ran in %ss with args: %s",
-                    func.__name__,
-                    f"{(end-start)/NANOS_PER_SECOND:9f}",
-                    repr((args, kwargs))[:repr_length],
-                )
-                return result
-            logger.log(
-                level,
-                "%s ran in %ss",
-                func.__name__,
-                f"{(end-start)/NANOS_PER_SECOND:9f}",
-            )
-            return result
+#     def decorator(func):
+#         @wraps(func)
+#         def wrapper(*args, **kwargs):
+#             start = perf_counter_ns()
+#             result = func(*args, **kwargs)
+#             end = perf_counter_ns()
+#             if repr_length == -1:
+#                 logger.log(
+#                     level,
+#                     "%s ran in %ss with args: %s",
+#                     func.__name__,
+#                     f"{(end-start)/NANOS_PER_SECOND:9f}",
+#                     repr((args, kwargs)),
+#                 )
+#                 return result
+#             if repr_length > 0:
+#                 logger.log(
+#                     level,
+#                     "%s ran in %ss with args: %s",
+#                     func.__name__,
+#                     f"{(end-start)/NANOS_PER_SECOND:9f}",
+#                     repr((args, kwargs))[:repr_length],
+#                 )
+#                 return result
+#             logger.log(
+#                 level,
+#                 "%s ran in %ss",
+#                 func.__name__,
+#                 f"{(end-start)/NANOS_PER_SECOND:9f}",
+#             )
+#             return result
 
-        return wrapper
+#         return wrapper
 
-    return decorator
+#     return decorator
 
 
 # Below are timer utilities refactored to have sharable code.
